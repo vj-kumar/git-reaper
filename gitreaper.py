63155a22edc5707d795478e3d6c57fed58da04bd
Vijai Kumar K <vijaikumar.kanagarajan@gmail.com>
2020-02-17 18:19:14 +0530

    Add option to sync fork with upstream
    ./gitreaper.py -c <configfile> -p <project> --sync
    
    Signed-off-by: Vijai Kumar K <vijaikumar.kanagarajan@gmail.com>

--- gitreaper.py
+++ gitreaper.py
@@ -9,44 +9,63 @@
 
 import os
 import sys
+import yaml
+import tempfile
+import shutil
+import subprocess
 from argparse import ArgumentParser
+from contextlib import contextmanager
 
 class GitReaper(object):
-    def __init__(self, argv):
-        self.config_file = ""
-        self.project = ""
-        self.__setupArgParser()
-        self.__parserArgs(argv)
+    def __init__(self):
+        return
 
-    def __setupArgParser(self):
-        self.__parser = ArgumentParser()
-        self.__parser.add_argument('-c', "--config", dest='config_file',
-                                    help="Master configuration file",
-                                    action='store',
-                                    required=True)
-        self.__parser.add_argument('-p', "--project", dest='project',
-                                    help="Project to work on",
-                                    action='store',
-                                    required=True)
-        self.__parser.add_argument("--sync",
-                                    help="Sync fork with upstream",
-                                    action='store_true',
-                                    required=False)
+    def parseConfigFile(self, configfile):
+        with open(os.path.abspath(configfile)) as file:
+            return yaml.load(file, Loader=yaml.FullLoader)
 
-    def __parserArgs(self, argv):
-        argv = self.__parser.parse_args()
-        self.config_file = argv.config_file
-        self.project = argv.project
-        self.command =argv.sync
+    @contextmanager
+    def directoryAs(self, path):
+        oldpwd=os.getcwd()
+        os.chdir(path)
+        try:
+            yield
+        finally:
+            os.chdir(oldpwd)
 
-    def execute(self):
-        print(self.config_file)
-        print(self.project)
-        print(self.command)
+    def sync(self, configfile, project):
+        ymlconfig = ""
+        tempdir = tempfile.mkdtemp(prefix='gitreaper-')
+        ymlconfig = self.parseConfigFile(configfile)
+        with self.directoryAs(tempdir):
+            cmd = 'git clone --mirror ' + ymlconfig[project]['upstream'] + ' .'
+            subprocess.call(cmd, shell=True)
+            cmd = 'git push -f -u ' + ymlconfig[project]['url']
+            subprocess.call(cmd + " --all", shell=True)
+            subprocess.call(cmd + " --tags", shell=True)
+        shutil.rmtree(tempdir)
 
-def main(argv):
-    reaper = GitReaper(argv)
-    reaper.execute()
+    def sendPatchUpstream(self, configfile, project):
+        return
+
+def main():
+    reaper = GitReaper()
+    parser = ArgumentParser()
+    parser.add_argument('-c', "--config", dest='config_file',
+                                help="Master configuration file",
+                                action='store',
+                                required=True)
+    parser.add_argument('-p', "--project", dest='project',
+                                help="Project to work on",
+                                action='store',
+                                required=True)
+    parser.add_argument("--sync",
+                                help="Sync fork with upstream",
+                                action='store_true',
+                                required=False)
+    args = parser.parse_args()
+    if args.sync:
+        reaper.sync(args.config_file, args.project)
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
