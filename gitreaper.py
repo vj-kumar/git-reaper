vj-kumar/git-reaper

commit 8f845a5a7660de9b4704f825789ecf1386654317

Add option to sync all repos
Add option to sync all repos specified in the config file.

Signed-off-by: Vijai Kumar K <vijaikumar.kanagarajan@gmail.com>

gitreaper.py
@@ -86,9 +86,7 @@ class GitReaper(object):
         with open(os.path.abspath(configfile)) as file:
             return yaml.load(file, Loader=yaml.FullLoader)
 
-    def sync(self, configfile, project, reverse):
-        ymlconfig = ""
-        ymlconfig = self.parseConfigFile(configfile)
+    def _repo_sync(self, project, ymlconfig):
         upstream_repo = ymlconfig[project]["upstream"]
         downstream_repo = ymlconfig[project]["url"]
         flags = ymlconfig[project].get("flags") or ""
@@ -142,6 +140,15 @@ class GitReaper(object):
         else:
             sync_entire_repo(project, upstream_repo, downstream_repo)
 
+    def sync(self, configfile, project, reverse):
+        ymlconfig = ""
+        ymlconfig = self.parseConfigFile(configfile)
+        if project is None:
+            for project_entry in ymlconfig:
+                self._repo_sync(project_entry, ymlconfig)
+        else:
+            self._repo_sync(project, ymlconfig)
+
     def sendPatchUpstream(self, configfile, project):
         return
 
@@ -154,14 +161,6 @@ def main():
         "--config",
         dest="config_file",
         help="Master configuration file",
-        action="store",
-        required=True,
-    )
-    parser.add_argument(
-        "-p",
-        "--project",
-        dest="project",
-        help="Project to work on",
         action="store",
         required=True,
     )
@@ -180,6 +179,20 @@ def main():
                 """,
         action="store_true",
         required=False,
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "-p",
+        "--project",
+        dest="project",
+        help="Project to work on",
+        action="store",
+    )
+    group.add_argument(
+        "-a",
+        "--all",
+        help="Sync all projects",
+        action="store_true",
     )
     args = parser.parse_args()
     if args.sync:

commit 3e5c7e3a62e9a511e783a44b386d6123e87df55e

Fix reverse not defined error
Broken by 8f845a5a76

Fixes the below error:
File "/srv/projects/git-reaper/./gitreaper.py", line 97, in _repo_sync
    if reverse:
NameError: name 'reverse' is not defined. Did you mean: 'reversed'?

Signed-off-by: Vijai Kumar K <vijaikumar.kanagarajan@gmail.com>

gitreaper.py
@@ -86,7 +86,7 @@ class GitReaper(object):
         with open(os.path.abspath(configfile)) as file:
             return yaml.load(file, Loader=yaml.FullLoader)
 
-    def _repo_sync(self, project, ymlconfig):
+    def _repo_sync(self, project, ymlconfig, reverse):
         upstream_repo = ymlconfig[project]["upstream"]
         downstream_repo = ymlconfig[project]["url"]
         flags = ymlconfig[project].get("flags") or ""
@@ -145,9 +145,9 @@ class GitReaper(object):
         ymlconfig = self.parseConfigFile(configfile)
         if project is None:
             for project_entry in ymlconfig:
-                self._repo_sync(project_entry, ymlconfig)
+                self._repo_sync(project_entry, ymlconfig, reverse)
         else:
-            self._repo_sync(project, ymlconfig)
+            self._repo_sync(project, ymlconfig, reverse)
 
     def sendPatchUpstream(self, configfile, project):
         return